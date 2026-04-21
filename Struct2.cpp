a2a0b6efbb6ad5781203be6b815a724a8d450013
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-04-21 10:30:26 +0700

    Struktur Bersarang

---
+++ Struct2.cpp
@@ -0,0 +1,17 @@
+
+
+struct DetailAlamat {
+    string desa; //village
+    string kota;//city
+};
+
+struct mahasiswa {
+    string NIM;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+
+
+
