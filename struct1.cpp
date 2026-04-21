4a1b11c9e1efff42dc59b5e45f663616d674271d
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-04-21 10:09:19 +0700

    Proses Pengambilan Data (Input)

--- struct1.cpp
+++ struct1.cpp
@@ -1,6 +1,13 @@
 
+    cout << "nomor Mahasiswa : ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
 
-int main(){
-    mahasiswa mhs;
+
 
 
