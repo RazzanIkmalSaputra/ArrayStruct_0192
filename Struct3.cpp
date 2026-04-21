836f9094d113476831a6f933ba310d609f761b9a
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-04-21 10:53:18 +0700

    kodingan struct 3

--- Struct3.cpp
+++ Struct3.cpp
@@ -1,5 +1,44 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+struct DetailAlamat {
+    string desa; //village
+    string kota;//city
+};
+
+struct mahasiswa {
+    string NIM;
+    string nama;
+    DetailAlamat alamat;
+    int umur;
+};
+
+int main(){
+    mahasiswa mhs;
+    cout << "nomor Mahasiswa : ";
+    getline(cin, mhs.NIM);
+    cout << "Nama Mahasiswa : ";
+    getline(cin, mhs.nama);
+    cout << "Alamat Mahasiswa : " << endl;
+    cout << "\t nama desa: ";
     cin >> mhs.alamat.desa;
+    cout << "\t nama kota: ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << " \nNIM : " << mhs.NIM;
+    cout << " \nNama : " << mhs.nama;
+    cout << " \nAlamat : " ;
+    cout << " \nDesa : " << mhs.alamat.desa;
+    cout << " \nKota : " << mhs.alamat.kota;
+    cout << " \nUmur : " << mhs.umur;  
+
+    return 0;
+}
 
 
 
