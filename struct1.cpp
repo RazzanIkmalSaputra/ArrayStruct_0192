82989afeccf54929cf751667a8ed59641c447b81
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2026-04-21 10:52:52 +0700

    kodingan struct1

--- struct1.cpp
+++ struct1.cpp
@@ -8,6 +8,22 @@ struct mahasiswa {
     int umur;
 };
 
+int main(){
+    mahasiswa mhs;
+    cout << "nomor Mahasiswa : ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
 
+    cout << endl;
+    cout << " \nNIM : " << mhs.NIM;
+    cout << " Nama : " << mhs.nama;
+    cout << "Alamat : " << mhs.alamat;
+    cout << "Umur : " << mhs.umur;  
+}
 
 
